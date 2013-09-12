81302a24b7c54f804f9f7ae8846499645135e6e5
Ian Wojtowicz <ian@miscellaneousprojects.com>
2013-09-12 09:39:23 -0700

    Initial commit.

---
+++ randomvancouver.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import random
+
+print "http://maps.google.com/maps?z=14&t=m&q=loc:" + str(random.uniform(49.3208494, 49.1881387)) + "," + str(random.uniform(-123.2880123, -122.8656566))
+
