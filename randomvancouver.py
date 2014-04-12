ef9d5bd52f99e74c04dd94f5d02efd3a0dae650e
Ian Wojtowicz <ian@miscellaneousprojects.com>
2014-04-11 18:41:23 -0700

    opens browser window in ubuntu.

--- randomvancouver.py
+++ randomvancouver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import random
+import os
 
-print "http://maps.google.com/maps?z=14&t=m&q=loc:" + str(random.uniform(49.3208494, 49.1881387)) + "," + str(random.uniform(-123.2880123, -122.8656566))
+os.system("xdg-open https://www.google.com/maps/@" + str(random.uniform(49.3208494, 49.1881387)) + "," + str(random.uniform(-123.2880123, -122.8656566)) + ",14z")
 
